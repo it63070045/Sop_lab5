24e7ad154b022b7923a55fab2e3381b5574d3673
Natanon Hanchana <natanon@Natanons-MacBook-Pro.local>
2022-09-20 11:40:42 +0700

    lab05

---
+++ src/main/java/com/example/lab05/Sentence.java
@@ -0,0 +1,10 @@
+package com.example.lab05;
+
+import java.util.ArrayList;
+
+public class Sentence {
+    public ArrayList<String> badSentences = new ArrayList<>(), goodSentences = new ArrayList<>();
+    public Sentence(){
+
+    }
+}
